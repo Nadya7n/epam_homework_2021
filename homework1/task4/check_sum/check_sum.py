3f67fb3c662e7ee84c0bf6eb204f2c59a0512c07
Nadya7n <nadya7n@bk.ru>
2021-06-25 12:34:17 +0300

    Update(4) homework1

--- homework1/task4/check_sum/check_sum.py
+++ homework1/task4/check_sum/check_sum.py
@@ -1,11 +1,9 @@
-from typing import List
 from collections import Counter
+from typing import List
 
 
-def check_sum_of_four(a: List[int],
-                      b: List[int],
-                      c: List[int],
-                      d: List[int]) -> int:
+def check_sum_of_four(a: List[int], b: List[int],
+                      c: List[int], d: List[int]) -> int:
     dictionary_of_sums = Counter()
     for element_a in a:
         for element_b in b:

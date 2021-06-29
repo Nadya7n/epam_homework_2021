8c55ba36072eabedd17de52846248771231a427c
Nadya7n <nadya7n@bk.ru>
2021-06-30 00:31:50 +0300

    Update(8) hw1

--- tests/hw1/task3_test.py
+++ tests/hw1/task3_test.py
@@ -3,13 +3,21 @@ import pytest
 from homework1.task3.min_max import find_maximum_and_minimum
 
 
-def test_correcting_work():
+def test_correct_work():
     """Testing that finding maximum and minimum is correct"""
+    my_file = open("task3_file1.txt", "w+")
+    my_file.write("45")
+    my_file.write("99")
+    my_file.write("104")
+    my_file.close()
     assert find_maximum_and_minimum("task3_file1.txt")
 
 
-def test_correcting_work_1_line():
+def test_correct_work_1_line():
     """Testing that finding maximum and minimum
     is correct when number of line = 1
     """
+    my_file = open("task3_file2.txt", "w+")
+    my_file.write("1")
+    my_file.close()
     assert find_maximum_and_minimum("task3_file2.txt")

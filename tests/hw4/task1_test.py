b0cb8bd4a867afe4391986a93b78328a980c82de
Nadya7n <nadya7n@bk.ru>
2021-08-21 17:19:09 +0300

    Edit task1_test.py

--- tests/hw4/task1_test.py
+++ tests/hw4/task1_test.py
@@ -12,25 +12,20 @@ def test_file(param) -> str:
     temp_file = f"{temp_dir}/test.txt"
     with open(temp_file, "w") as f:
         f.write(param)
-    return temp_file
+    yield temp_file
+    os.remove(temp_file)
 
 
 @pytest.mark.parametrize("param", ["1", "2"])
 def test_reading_magic_number_positive_case(test_file, param):
     assert read_magic_number(test_file) is True
-    os.remove("/tmp/test.txt")
 
 
 @pytest.mark.parametrize("param", ["12", "are", ""])
 def test_reading_magic_number_negative_case(test_file, param):
     assert read_magic_number(test_file) is False
-    os.remove("/tmp/test.txt")
 
 
 def test_reading_magic_number_file_error():
-    try:
+    with pytest.raises(ValueError):
         read_magic_number("test.txt")
-    except ValueError:
-        assert True
-    else:
-        assert False

12ccf7c6cca5bc1784baf0d630d87580ed693e02
Nadezhda Sorokina <71948290+Nadya7n@users.noreply.github.com>
2021-08-23 02:31:58 +0300

    Create tests

--- homework12/task/migration/tests.py
+++ homework12/task/migration/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from migration.models import *
+
+
+class AnimalTestCase(TestCase):
+    def test_presence(self):
+        hw = Homework.objects.get(text="Calculate integral")
+        hw_res = HomeworkResult.objects.get(solution="Done")
+        teacher = Teacher.objects.get(last_name="Wonderful")
+        student = Student.objects.get(first_name="Olya")
+
+        self.assertEqual(hw.text, "Calculate integral")
+        self.assertEqual(hw_res.solution, "Done")
+        self.assertEqual(teacher.last_name, "Wonderful")
+        self.assertEqual(student.first_name, "Olya")

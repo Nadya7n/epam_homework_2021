8a7df9bcb71e47533ade8919513a2d9bc02407ab
Nadezhda Sorokina <71948290+Nadya7n@users.noreply.github.com>
2021-08-24 02:02:13 +0300

    remove homework12  из пути "name"

--- homework12/task/migration/apps.py
+++ homework12/task/migration/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MigrationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "homework12.task.migration"
+    name = "task.migration"

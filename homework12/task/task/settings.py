deb2aac0f94809954b2f29bbd00d223550894136
Nadezhda Sorokina <71948290+Nadya7n@users.noreply.github.com>
2021-08-24 02:12:17 +0300

    edit settings.py (2)

--- homework12/task/task/settings.py
+++ homework12/task/task/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "task.migration.apps.MigrationConfig",
+    "homework12.task.migration.apps.MigrationConfig",
 ]
 
 MIDDLEWARE = [

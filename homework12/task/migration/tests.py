2e88a3ce22bd1de3d0b8d772a128bf51489d4cdb
Nadezhda Sorokina <71948290+Nadya7n@users.noreply.github.com>
2021-08-23 23:37:52 +0300

    Change tests

--- homework12/task/migration/tests.py
+++ homework12/task/migration/tests.py
@@ -1,28 +1,10 @@
-import datetime
-
 from django.test import TestCase
 from migration.models import *
 
 
 class ModelsTestCase(TestCase):
-    def setUp(self):
-        Teacher.objects.create(first_name="Ivan", last_name="Wonderful")
-
-        Student.objects.create(first_name="Olya", last_name="Yalo")
-
-        Homework.objects.create(
-            text="Calculate integral", deadline=datetime.timedelta(days=7), teacher_id=1
-        )
-
-        HomeworkResult.objects.create(hw_id=1, solution="Done", student_id=1)
-
     def test_presence(self):
-        hw = Homework.objects.get(id=1)
-        hw_res = HomeworkResult.objects.get(id=1)
-        teacher = Teacher.objects.get(id=1)
-        student = Student.objects.get(id=1)
-
-        self.assertEqual(hw.text, "Calculate integral")
-        self.assertEqual(hw_res.solution, "Done")
-        self.assertEqual(teacher.last_name, "Wonderful")
-        self.assertEqual(student.first_name, "Olya")
+        assert Teacher.objects.all().count() == 1
+        assert Student.objects.all().count() == 1
+        assert Homework.objects.all().count() == 1
+        assert HomeworkResult.objects.all().count() == 1

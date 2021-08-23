c771e5f9f40a764d95362de8b9882dfd86df360d
Nadezhda Sorokina <71948290+Nadya7n@users.noreply.github.com>
2021-08-24 02:09:27 +0300

    Edit manage.py

--- homework12/task/manage.py
+++ homework12/task/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homework12.task.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

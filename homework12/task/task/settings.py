7011f06dfe411d74201f511bf330c8f24d146bc3
Nadezhda Sorokina <71948290+Nadya7n@users.noreply.github.com>
2021-08-24 02:03:54 +0300

    Try to make test. 4

--- homework12/task/task/settings.py
+++ homework12/task/task/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "homework12.task.migration.apps.MigrationConfig",
+    "task.migration.apps.MigrationConfig",
 ]
 
 MIDDLEWARE = [
